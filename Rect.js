4a026a83fc110ddf0729911ac36ea9ec45fb3d9f
Aarushi2704 <57027602+Aarushi2704@users.noreply.github.com>
2020-04-20 21:10:31 +0530

    Add files via upload

--- Rect.js
+++ Rect.js
@@ -4,4 +4,18 @@ class Rect extends BaseClass{
     this.image = loadImage("sprites/block1.png");
     Matter.Body.setAngle(this.body, angle);
   }
+  display(){
+    if(this.body.speed < 3){
+      super.display();
+    }
+    else{
+      World.remove(world, this.body);
+      push();
+      this.Visibility = this.Visibility - 5;
+      tint(255, this.Visibility);
+      image(this.image, this.body.position.x, this.body.position.y,50,50);
+      pop();
+
+    }
+  }
 }
